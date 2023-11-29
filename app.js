5f482c5f423fbf4c6edfecac87b5418bf39e537e
Isaac Ayomide <okunlolaa3@outlook.com>
2023-11-29 11:28:40 +0100

    add desktop share modal

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const activeSection = document.querySelector('.active');
 const inactiveShareIcon = document.querySelector('.inactive-share-icon');
 const exitShare = document.querySelector('.share-icon');
 
+//Desktiop share Icon
+const desktopShareIcon = document.querySelector('.desktop-share-icon')
+const desktopModal = document.querySelector('.hide-modal')
+const revealModal = document.querySelector('.displayNone')
+// console.log(desk)
+
 inactiveShareIcon.addEventListener('click', function () {
     if (!inactiveSection.classList.contains('hidden')) {
         inactiveSection.classList.add('hidden')
         activeSection.classList.remove('hidden')
+    } else {
+        inactiveSection.classList.remove('hidden')
+        activeSection.classList.add('hidden')
     }
 })
 
@@ -17,6 +26,11 @@ exitShare.addEventListener('click', function () {
     }
 })
 
-// document.addEventListener('click', function () {
-//
-// })
+desktopShareIcon.addEventListener('click', function () {
+    if (desktopModal.classList.contains('hide-modal')) {
+        desktopModal.classList.remove('hide-modal')
+        revealModal.style.display = "block"
+    } else {
+        desktopModal.classList.add('hide-modal')
+    }
+})
